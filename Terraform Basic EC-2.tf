2cb6cd14c6bfe36f06224c977f81ed8b90dec712
Azzy-Dev <118063625+Arzianghanchi@users.noreply.github.com>
2024-07-18 15:06:53 +0530

    Update Terraform Basic EC-2.tf

--- Terraform Basic EC-2.tf
+++ Terraform Basic EC-2.tf
@@ -1,7 +1,7 @@
 # Create an EC2 Instance
 resource "aws_instance" "terraformins" {
-  ami           = "ami-04a81a99f5ec58529"   
-  instance_type = "t2.micro"                
+  ami           = "ami-04a81a99f5ec58529"   # change the ami id 
+  instance_type = "t2.micro"   # change the instance_type as per requirement            
 
   tags = {
     Name = "server1"
